49d092ad519815cc6215ed841be8aedc915e9e2f
gamonpo <gamonpo@outlook.com>
2020-05-12 09:47:42 -0300

    style(Main): componentizando style da view

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,64 +1,45 @@
-import React from 'react';
-import { View, Image, Text, Button } from 'react-native';
+import React, { Component } from 'react';
+
+import { Text, View, Button } from 'react-native';
+
+import {
+  ViewMain,
+  Logo1,
+  Logo2,
+  GreetMessage,
+  MessageOne,
+  MessageTwo,
+  LoginButtonView,
+  SignUpText,
+} from './styles';
 
 import logo1 from '../../assets/logo1.jpg';
 import logo2 from '../../assets/logo2.jpg';
 
-function Main({ navigation }) {
-  return (
-    <>
-      <View
-        style={{
-          flex: 1,
-          height: '100%',
-          alignItems: 'center',
-          justifyContent: 'center',
-          backgroundColor: 'black',
-        }}
-      >
-        <Image
-          source={logo1}
-          style={{
-            resizeMode: 'contain',
-            width: 400,
-            height: 400,
-            marginTop: -100,
-          }}
-        />
-        <Image
-          source={logo2}
-          style={{
-            resizeMode: 'contain',
-            width: 300,
-            height: 200,
-            marginTop: -100,
-          }}
-        />
-        <View style={{ flexDirection: 'column', alignItems: 'center' }}>
-          <Text style={{ color: '#FDB32A', fontSize: 28 }}>
-            Greetings Friend!
-          </Text>
-          <Text style={{ color: '#FDB32A', fontSize: 28 }}>Welcome...</Text>
-        </View>
-        <View
-          style={{
-            width: '100%',
-            height: 150,
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <Button title="Login" color="#1749CA" />
-        </View>
+export default class Main extends Component {
+  render() {
+    return (
+      <>
+        <ViewMain>
+          <Logo1 source={logo1} />
+          <Logo2 source={logo2} />
+          <GreetMessage>
+            <MessageOne>Greetings Friend!</MessageOne>
 
-        <View>
-          <Button title="Create Account" color="#1749CA" />
-        </View>
+            <MessageTwo>Welcome....</MessageTwo>
+          </GreetMessage>
 
-        <Text style={{ color: '#FFFFFF' }}>Don't have an account? Sign Up</Text>
-      </View>
-    </>
-  );
+          <LoginButtonView>
+            <Button title="Login" color="#1749CA" />
+          </LoginButtonView>
+
+          <View>
+            <Button title="Create Account" color="#1749CA" />
+          </View>
+
+          <SignUpText>Don't have an account? Sign Up</SignUpText>
+        </ViewMain>
+      </>
+    );
+  }
 }
-
-export default Main;
---
+++ src/pages/Main/styles.js
@@ -0,0 +1,53 @@
+/* eslint-disable prettier/prettier */
+import styled from 'styled-components/native';
+
+export const ViewMain = styled.View`
+  flex: 1;
+  height: 100%;
+  alignItems: center;
+  justifyContent: center;
+  background: black;
+`;
+
+export const Logo1 = styled.Image`
+  resizeMode: contain;
+  width: 400;
+  height: 400;
+  margin-top: -100;
+`;
+
+export const Logo2 = styled.Image`
+  resizeMode: contain;
+  width: 300;
+  height: 200;
+  margin-top: -100;
+`;
+
+export const GreetMessage = styled.View`
+  flex-direction: column;
+  alignItems: center;
+`;
+
+export const MessageOne = styled.Text`
+  color: #FDB32A;
+  fontSize: 28;
+`;
+
+export const MessageTwo = styled.Text`
+  color: #FDB32A;
+  fontSize: 28;
+`;
+
+export const LoginButtonView = styled.View`
+  width: 100%;
+  height: 100;
+  justify-content: center;
+  alignItems: center;
+
+`;
+
+export const SignUpText = styled.Text`
+  color: #FFFF;
+`;
+
+
