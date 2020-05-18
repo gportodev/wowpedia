70a178b050ac80378d2e0f8cbaa96f4a21b30f32
gamonpo <gamonpo@outlook.com>
2020-05-18 16:56:30 -0300

    style(CreateAccount): estilizacao da tela

--- src/pages/CreateAccount/index.js
+++ src/pages/CreateAccount/index.js
@@ -1,10 +1,82 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Button, KeyboardAvoidingView } from 'react-native';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Header,
+  Message,
+  List,
+  Logo1,
+  Logo2,
+  Logo3,
+  Form,
+  NickView,
+  EmailView,
+  Border,
+  PasswordView,
+  Input,
+  CreateAccountView,
+} from './styles';
+
+import wow from '../../assets/logo1.jpg';
+import horde from '../../assets/horde.jpg';
+import ally from '../../assets/ally.jpg';
 
 const CreateAccount = () => {
-  return <View />;
+  return (
+    <Container>
+      <List>
+        <Logo2 source={horde} />
+        <Logo3 source={ally} />
+      </List>
+
+      <KeyboardAvoidingView>
+        <Form>
+          <NickView>
+            <Input
+              style={{ color: '#FFFF' }}
+              autoCorrect={false}
+              autoCapitalize="none"
+              placeholder="Name"
+            />
+          </NickView>
+
+          <Border />
+
+          <EmailView>
+            <Input
+              style={{ color: '#FFFF' }}
+              autoCorrect={false}
+              autoCapitalize="none"
+              placeholder="Email adress"
+            />
+          </EmailView>
+
+          <Border />
+
+          <PasswordView>
+            <Input
+              style={{ color: '#FFFF' }}
+              autoCorrect={false}
+              autoCapitalize="none"
+              placeholder="Password"
+              secureTextEntry
+            />
+          </PasswordView>
+
+          <Border />
+        </Form>
+      </KeyboardAvoidingView>
+
+      <CreateAccountView>
+        <Button title="Create Account" color="#1749CA" />
+      </CreateAccountView>
+
+      <Header>
+        <Message>The drums of war thunder once again...</Message>
+      </Header>
+    </Container>
+  );
 };
 
 export default CreateAccount;
---
+++ src/pages/CreateAccount/styles.js
@@ -0,0 +1,81 @@
+/* eslint-disable prettier/prettier */
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  background: black;
+`;
+
+
+
+export const List = styled.View`
+  flex-direction: row;
+  justify-content: center;
+  margin-top: 10;
+`;
+
+export const Logo2 = styled.Image`
+  /* resizeMode: contain; */
+  width: 200;
+  height: 200;
+  /* background: red */
+`;
+
+export const Logo3 = styled.Image`
+  /* resizeMode: contain; */
+  width: 200;
+  height: 200;
+  /* background: blue */
+`;
+
+export const Form = styled.View`
+  margin-top: -20;
+`;
+
+export const NickView = styled.View`
+  margin-top: 50px;
+`;
+
+
+export const EmailView = styled.View`
+  margin-top: 50px;
+`;
+
+export const PasswordView = styled.View`
+  margin-top: 50px;
+`;
+
+export const Input = styled.TextInput.attrs({
+  placeholderTextColor: '#FFFFFF',
+})``;
+
+
+export const Border = styled.View`
+  border-bottom-color: #f8f8f8;
+  border-bottom-width: 1;
+`;
+
+export const LoginButtonView = styled.View`
+  width: 150;
+  padding-top: 40;
+`;
+
+export const CreateAccountView = styled.View`
+  width: 100%;
+  padding-top: 50;
+  align-items: center;
+`;
+
+
+export const Header = styled.View`
+  align-self: center;
+  margin-top: 50;
+`;
+
+export const Message = styled.Text`
+  color: #FDB32A;
+  fontSize: 22;
+`;
+
+
+
