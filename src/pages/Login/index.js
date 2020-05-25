1f404d304c3c736e0428301fe7ad3e85f97e78bc
gamonpo <gamonpo@outlook.com>
2020-05-25 17:19:16 -0300

    feat(Login): pula para o proximo input depois do submit

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Button } from 'react-native';
 
@@ -6,25 +6,41 @@ import { Container, Logo, Form, Input, ButtonView } from './styles';
 
 import logo3 from '../../assets/logos/logo3.jpg';
 
-const Login = () => {
+export default function Login() {
+  const [state, setState] = useState({
+    email: '',
+    password: '',
+  });
+
+  let passwordInput = '';
+
   return (
     <Container>
       <Logo source={logo3} />
       <Form>
         <Input
+          value={state.email}
+          onChangeText={(text) => setState({ email: text })}
           autoCorrect={false}
           autoCapitalize="none"
           returnKeyType="next"
           placeholder="Email"
           placeholderTextColor="#65279"
+          autoFocus
+          onSubmitEditing={() => passwordInput.focus()}
         />
 
         <Input
+          value={state.password}
+          onChangeText={(text) => setState({ password: text })}
           autoCorrect={false}
           secureTextEntry
           autoCapitalize="none"
           placeholder="Password"
           placeholderTextColor="#65279"
+          ref={(el) => {
+            passwordInput = el;
+          }}
         />
       </Form>
 
@@ -33,6 +49,4 @@ const Login = () => {
       </ButtonView>
     </Container>
   );
-};
-
-export default Login;
+}
